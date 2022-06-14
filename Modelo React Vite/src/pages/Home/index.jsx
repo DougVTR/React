5b31883b3dc9a6e5f4a13c26eeb0e3c7e6e3b5a5
Douglas <douglasvtr1999@gmail.com>
2022-06-14 08:25:14 -0300

    Realocação de arquivos

---
+++ Modelo React Vite/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+export function Home() {
+  return ( 
+    <div className="app"> 
+      <h1>Olá mundo!</h1>
+      <input type="text" name="nome" id="nome" placeholder="Digite seu nome..." />
+      <button>Enviar</button>
+    </div> 
+  )
+}
