4b069381a2418b317bc393f4bfe3ad7477148ad4
Douglas <douglasvtr1999@gmail.com>
2022-07-08 13:32:43 -0300

    Remoção de pasta de modelos

--- Modelos/Modelo React Vite/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export function Home() {
-  return ( 
-    <div className="app"> 
-      <h1>Olá mundo!</h1>
-      <input type="text" name="nome" id="nome" placeholder="Digite seu nome..." />
-      <button>Enviar</button>
-    </div> 
-  )
-}
