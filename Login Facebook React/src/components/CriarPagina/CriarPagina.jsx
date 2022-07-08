b682b721560df431750d702f716fcd5dd1c50931
Douglas <douglasvtr1999@gmail.com>
2022-07-08 13:24:41 -0300

    Criação de projeto da página do Facebook no modo escuro

---
+++ Login Facebook React/src/components/CriarPagina/CriarPagina.jsx
@@ -0,0 +1,10 @@
+import './criarPagina.css'
+export function CriarPagina() {
+    return (
+
+        <div id="criarPagina"> {/*Texto abaixo do menu de login*/}
+            <p><a id='criarPLINK' href="/">Criar uma Página</a> para uma celebridade, uma marca ou uma </p>
+            <p> empresa.</p>
+        </div>
+    )
+}
---
+++ Login Facebook React/src/components/InfoLogo/InfoLogo.jsx
@@ -0,0 +1,10 @@
+import './infoLogo.css'
+export function InfoLogo() {
+    return (
+        <section className='infoSection'>
+            <div className="info"> {/* Texto de apresentação inicial da página*/}
+                <h1>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</h1>
+            </div>
+        </section>
+    )
+}
